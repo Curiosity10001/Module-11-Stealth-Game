092680c2943b83a228b4b87d6c8137296d8ea22d
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-10-27 01:29:07 +0200

    Ongroundcheck/transfer with ray cast , jump broken

--- Stealth Game work files/Assets/Scripts/PlayerMovement.cs
+++ Stealth Game work files/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,45 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector3 moveDirection;
     Rigidbody rgbd;
+
+    [Header (" Public Parameter for multi-scrip")]
     public float timer = 0;
 
+    [Header ("Movement parameters")]
     [SerializeField] float speed = 5f; 
-    [SerializeField] float jumpForce= 5f;
-    [SerializeField] float rotationSpeed = 5f;
-    [SerializeField] Transform groundCheck;
-
-    
-    public Animator animator;
+    [SerializeField] float rotationSpeed = 60f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] bool isJumping = false;
+  
     float lastJump;
-    float deltaTimeJump =1f;
+    float deltaTimeJump = 1f;
+
+    [Header("Floor Detection")]
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] float maxDistance = 1.9f;
+    [SerializeField] Transform frontRightRay;
+    [SerializeField] Transform frontLeftRay;
+    [SerializeField] Transform backRightRay;
+    [SerializeField] Transform backLeftRay;
+    
+    Vector3 onTheGround;   
+    RaycastHit touchGround1;
+    RaycastHit touchGround2;
+    RaycastHit touchGround3;
+    RaycastHit touchGround4;
+    Ray rayToGround1;
+    Ray rayToGround2;
+    Ray rayToGround3;
+    Ray rayToGround4;
+    bool HitGround1;
+    bool HitGround2;
+    bool HitGround3;
+    bool HitGround4;
+
+
+
+    public Animator animator;
+   
 
 
 
@@ -38,11 +66,21 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection =  Camera.main.transform.forward * Input.GetAxis("Vertical") + Camera.main.transform.right * Input.GetAxis("Horizontal") ;
         timer += Time.deltaTime;
+
+        Grounded();
+        if (rgbd.position.y <= onTheGround.y + 0.1f && rgbd.position.y >= onTheGround.y - 0.1f)
+        {
+            isGrounded = true;
+            isJumping = false;
+        }
+        else isGrounded = false;
+
         if (timer >= lastJump + deltaTimeJump)
         {
 
-            if (Input.GetButtonDown("Jump") && onGround() )
+            if (Input.GetButtonDown("Jump") && isGrounded)
             {
+                
                 Jump();
                 lastJump = Time.time;
               
@@ -50,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
             
         }
 
-
+        Debug.Log("rgbd y" + rgbd.position.y + " raycast Y " + onTheGround.y);
     }
     private void FixedUpdate()
     {
@@ -59,14 +97,12 @@ public class PlayerMovement : MonoBehaviour
         TurnToCamForwardDirection();
         
     }
-    bool onGround()
-    {
-        return Physics.CheckSphere(groundCheck.position, 0.01f, 3);
-    }
 
     void Jump()
     {
-       rgbd.AddForce(Vector2.up * jumpForce, ForceMode.Impulse) ;
+       rgbd.AddForce(Vector2.up * jumpForce) ;
+       isJumping = true;
+
     }
     void TurnToCamForwardDirection()
     {
@@ -78,7 +114,34 @@ public class PlayerMovement : MonoBehaviour
         Quaternion rgbdLookAt= Quaternion.LookRotation(CamForward);
         Quaternion rgbdRotationSpeed = Quaternion.RotateTowards(rgbd.rotation, rgbdLookAt, rotationSpeed * Time.fixedDeltaTime);
         rgbd.MoveRotation(rgbdLookAt);
+        rgbd.rotation = rgbdRotationSpeed;
         
 
     }
+
+    public void  Grounded()
+    {
+        
+        rayToGround1 = new Ray(frontRightRay.position, Vector3.down);
+        rayToGround2 = new Ray(frontLeftRay.position, Vector3.down);
+        rayToGround3 = new Ray(backRightRay.position, Vector3.down);
+        rayToGround4 = new Ray(backLeftRay.position, Vector3.down);
+
+        HitGround1 = Physics.Raycast(rayToGround1, out touchGround1, maxDistance, LayerMask.GetMask("Ground"));
+        HitGround2 = Physics.Raycast(rayToGround2, out touchGround2, maxDistance, LayerMask.GetMask("Ground"));
+        HitGround3 = Physics.Raycast(rayToGround3, out touchGround3, maxDistance, LayerMask.GetMask("Ground"));
+        HitGround4 = Physics.Raycast(rayToGround4, out touchGround4, maxDistance, LayerMask.GetMask("Ground"));
+        
+            if (HitGround1 && HitGround2 && HitGround3 && HitGround4)
+            {
+            onTheGround = (touchGround1.point + touchGround2.point+ touchGround3.point + touchGround4.point) / 4 ;
+            }
+        
+       
+        
+    }
+  
+
+
+
 }
